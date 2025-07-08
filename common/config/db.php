fabac7d29908035b594853881c5bec3d5578a198
root <root@optim184627.vds>
2025-07-08 22:08:44 +0000

    dell db.php

--- common/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=m4leads',
-    'username' => 'admin',
-    'password' => 'bgt5bhu8',
-    'charset' => 'utf8',
-];
