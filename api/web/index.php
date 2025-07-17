407b2d652ac7968c53c8effdd895f75d82560c4c
root <root@optim184627.vds>
2025-07-17 14:01:21 +0000

    еще один апдейт

--- api/web/index.php
+++ api/web/index.php
@@ -1,14 +1,27 @@
 <?php
 
+require(__DIR__ . '/../../vendor/autoload.php');
+
+use Dotenv\Dotenv;
+use api\components\TelegramNotifier;
+
+//.env
+try {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+    $dotenv->load();
+} catch (\Exception $e) {
+    $errorMessage = "❌ Ошибка загрузки .env: " . $e->getMessage();
+    TelegramNotifier::sendMessage($errorMessage);
+    error_log($errorMessage);
+     die("Произошла критическая ошибка. Проверьте файл .env.");
+}
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/main.php');
 
 $application = new yii\web\Application($config);
-
-
-    $application->run();
+$application->run();
--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,9 +1,28 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use api\components\TelegramNotifier;
+
+//.env
+try {
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+    $dotenv->load();
+} catch (\Exception $e) {
+    $errorMessage = "❌ Ошибка загрузки .env: " . $e->getMessage();
+    TelegramNotifier::sendMessage($errorMessage);
+    error_log($errorMessage);
+     die("Произошла критическая ошибка. Проверьте файл .env.");
+}
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';
 require __DIR__ . '/../config/bootstrap.php';
